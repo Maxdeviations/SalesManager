d42f4ee09de11e7d941ff860b6d9a89c56cf1837
Maxdeviations <maks.manin.2020@mail.ru>
2023-02-20 17:44:09 +0300

    Switch to long

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[] {6, 4, 3});
-        int max = salesManager.max();
+        SalesManager salesManager = new SalesManager(new long[] {6, 4, 3});
+        long max = salesManager.max();
         System.out.println(max);
     }
 }
